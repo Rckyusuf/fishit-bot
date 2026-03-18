91b72b867e4a2d6527d8d31e850f461d81213f28
Rckyusuf <rickyyusuf20@gmail.com>
2026-03-18 11:01:18 +0700

    Update fish_database.lua

--- fish_database.lua
+++ fish_database.lua
@@ -9,14 +9,61 @@ return {
     -- ═══════════════════════════════════
     --  Fisherman Island
     -- ═══════════════════════════════════
+    ["Old Boot"] = "https://tr.rbxcdn.com/180DAY-f2c3d23b9c074a3445431e71d0eb3d3b/420/420/Image/Png/noFilter",
+    ["Conch Shell"] = "https://tr.rbxcdn.com/180DAY-7f7b94e8829098633e798538a3bfe6a0/420/420/Image/Png/noFilter",
+    ["Sea Shell"] = "https://tr.rbxcdn.com/180DAY-ca43b8917c8e1789531c74f61ba75f67/420/420/Image/Png/noFilter",
+    ["Azure Damsel"] = "https://tr.rbxcdn.com/180DAY-ef7baecc225658f66998ffb574b3b482/420/420/Image/Png/noFilter",
+    ["Flame Angelfish"] = "https://tr.rbxcdn.com/180DAY-5c88a26706945ad859986574e2d1e407/420/420/Image/Png/noFilter",
+    ["Korean Angelfish"] = "https://tr.rbxcdn.com/180DAY-0f9a240b4afaeaaf48f9bf77b8fe5b66/420/420/Image/Png/noFilter",
+    ["Ballina Angelfish"] = "https://tr.rbxcdn.com/180DAY-a74176d2a2628ecb49b710729f8d7b7b/420/420/Image/Png/noFilter",
+    ["Yello Damselfish"] = "https://tr.rbxcdn.com/180DAY-6df8612810bc11c4d9b8a4502bf42325/420/420/Image/Png/noFilter",
+    ["Strawberry Dotty"] = "https://tr.rbxcdn.com/180DAY-746e7ff3c82be31f3052d4cea60ac7f9/420/420/Image/Png/noFilter",
+    ["Yellowfin Tuna"] = "https://tr.rbxcdn.com/180DAY-e35d0e0b952974d14d1811fbee00dd20/420/420/Image/Png/noFilter",
+    ["Copperband Butterfly"] = "https://tr.rbxcdn.com/180DAY-d41630feb35fae1dbf1206baaf1812a2/420/420/Image/Png/noFilter",
+    ["Darwin Clownfish"] = "https://tr.rbxcdn.com/180DAY-c550b09a515fc2d8f4f0f6a7d9042e9d/420/420/Image/Png/noFilter",
+    ["Clownfish"] = "https://tr.rbxcdn.com/180DAY-70efa988a569439d8858bea4390af194/420/420/Image/Png/noFilter",
+    ["Dorhey Tang"] = "https://tr.rbxcdn.com/180DAY-be70f4f4fe0cefe0d9e7a8228316c043/420/420/Image/Png/noFilter",
+    ["Unicorn Tang"] = "https://tr.rbxcdn.com/180DAY-5deb7e6fd454ded00d4a29c7dafb97f0/420/420/Image/Png/noFilter",
+    ["Dotted Stingray"] = "https://tr.rbxcdn.com/180DAY-0e52fce670e1b9c80815270d4e4fe811/420/420/Image/Png/noFilter",
+    ["Enchant Stone"] = "https://tr.rbxcdn.com/180DAY-bbbf9cb59b11ca334aa4c227a435ef44/420/420/Image/Png/noFilter",
+    ["Dogfish"] = "https://tr.rbxcdn.com/180DAY-925ede40db2556d662c5f3441cdc3ee1/420/420/Image/Png/noFilter",
+    ["Glacierfin Snapper"] = "https://tr.rbxcdn.com/180DAY-52c5571cf85d30c12e8b821972d16f8d/420/420/Image/Png/noFilter",
+    ["Bandit Angelfish"] = "https://tr.rbxcdn.com/180DAY-27887df27b7e9746358e38783756f37e/420/420/Image/Png/noFilter",
+    ["Lined Cardinal Fish"] = "https://tr.rbxcdn.com/180DAY-c9508d847cef3f83a16345352dfeeeda/420/420/Image/Png/noFilter",
+    ["Pygmy Goby"] = "https://tr.rbxcdn.com/180DAY-2c9613d1da9a086bc20987d0a720f00f/420/420/Image/Png/noFilter",
+    ["Watanabei Angelfish"] = "https://tr.rbxcdn.com/180DAY-2d53f544e5d03ea123c65c0dec63d744/420/420/Image/Png/noFilter",
+    ["White Tang"] = "https://tr.rbxcdn.com/180DAY-0f6d3b31451f8c552a290861876ae0c0/420/420/Image/Png/noFilter",
+    ["Herring Fish"] = "https://tr.rbxcdn.com/180DAY-47e7d268aad9e5526829d473703c7b28/420/420/Image/Png/noFilter",
+    ["Barracuda Fish"] = "https://tr.rbxcdn.com/180DAY-de2fa7de7acb5f2b7cb5d24d76a98f95/420/420/Image/Png/noFilter",
+    ["Gar Fish"] = "https://tr.rbxcdn.com/180DAY-7b698a7f4b63739fec9b516ee2ba028c/420/420/Image/Png/noFilter",
+    ["Frog"] = "https://tr.rbxcdn.com/180DAY-9e3bab1a22053b37881140ef308f1107/420/420/Image/Png/noFilter",
+    ["King Frog"] = "https://tr.rbxcdn.com/180DAY-aac75ed33955d81cec7778f2ddabbfa5/420/420/Image/Png/noFilter",
+    ["Lake Sturgeon"] = "https://tr.rbxcdn.com/180DAY-2e7631b1fa369e216e1abc6f77c0f160/420/420/Image/Png/noFilter",
+    ["Crystal Crab"] = "https://tr.rbxcdn.com/180DAY-644fdd8d9b29c3c4da83e170b9b3eab0/420/420/Image/Png/noFilter",
+    ["Orca"] = "https://tr.rbxcdn.com/180DAY-e3d4495262428ed404619d8e6f16d6ec/420/420/Image/Png/noFilter",
+
+
+    -- ═══════════════════════════════════
+    --  Treasure Room
+    -- ═══════════════════════════════════
     ["Ruby"] = "https://tr.rbxcdn.com/180DAY-91dd82d150765096695084e981e85d6f/420/420/Image/Png/noFilter",
-    -- Tambah ikan Fisherman Island lain di sini
-
-
-    -- ═══════════════════════════════════
-    --  Area 2 (ganti nama sesuai area)
-    -- ═══════════════════════════════════
-    -- ["Nama Ikan"] = "https://tr.rbxcdn.com/...",
+    ["Dead Fish"] = "https://tr.rbxcdn.com/180DAY-ae0d0dc3b6a6eb77c493c16e72744e09/420/420/Image/Png/noFilter",
+    ["Skeleton Fish"] = "https://tr.rbxcdn.com/180DAY-aada1bbfc99b18c35f0f70256a35a1c6/420/420/Image/Png/noFilter",
+    ["Swordfish"] = "https://tr.rbxcdn.com/180DAY-c42739e083166713fc660a008cddf094/420/420/Image/Png/noFilter",
+    ["Viperfish"] = "https://tr.rbxcdn.com/180DAY-2569bbc21d90fbbff503ef8074da1716/420/420/Image/Png/noFilter",
+    ["Vampire Squid"] = "https://tr.rbxcdn.com/180DAY-833e0587f6e34770e3ee53a2935facf3/420/420/Image/Png/noFilter",
+    ["Spotted Lantern Fish"] = "https://tr.rbxcdn.com/180DAY-b82ebc0545d9cfed0bc05f4cbf0bed31/420/420/Image/Png/noFilter",
+    ["Monk Fish"] = "https://tr.rbxcdn.com/180DAY-0b2fb9abf61fd8572667efb1764bfcdb/420/420/Image/Png/noFilter",
+    ["Jellyfish"] = "https://tr.rbxcdn.com/180DAY-a426f886a5eaee7874dfb7cd35faafbd/420/420/Image/Png/noFilter",
+    ["Synodontis"] = "https://tr.rbxcdn.com/180DAY-17143d92790473f6670a88769e38380d/420/420/Image/Png/noFilter",
+    ["Fangtooth"] = "https://tr.rbxcdn.com/180DAY-7fabfe504d9c9c8231c38f5f24ffaacc/420/420/Image/Png/noFilter",
+    ["Armor Catfish"] = "https://tr.rbxcdn.com/180DAY-d7c312fad8eaf07b3f8ec02f3bafb9e4/420/420/Image/Png/noFilter",
+    ["Blob Fish"] = "https://tr.rbxcdn.com/180DAY-aa1e7cbb92cdaf7ab8e145b0fbe06dbb/420/420/Image/Png/noFilter",
+    ["Angler Fish"] = "https://tr.rbxcdn.com/180DAY-31613ab5d2a1e897a4bd2aaf8f399e69/420/420/Image/Png/noFilter",
+    ["Deep Sea Crab"] = "https://tr.rbxcdn.com/180DAY-23b479fd34efe45a718518b4fc3dbe20/420/420/Image/Png/noFilter",
+    ["King Crab"] = "https://tr.rbxcdn.com/180DAY-759396ad59e6d31e9d6bf6a025288e4b/420/420/Image/Png/noFilter",
+    ["Queen Crab"] = "https://tr.rbxcdn.com/180DAY-75f017dc45d893c85ae39873590042b1/420/420/Image/Png/noFilter",
+    ["Antique Watch"] = "https://tr.rbxcdn.com/180DAY-0bd0f3ec8d3eddc654a4eb91122a1202/420/420/Image/Png/noFilter",
 
 
     -- ═══════════════════════════════════
